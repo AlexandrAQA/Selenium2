d9fb2a39bcc0fa279442a2865063b565f43d8602
AndroidPanaev <panaev.as@gmail.com>
2020-01-11 23:07:49 +0300

    Click on RadioButton(CheckBox) and check the Status

--- RadioButton.py
+++ RadioButton.py
@@ -1,11 +1,10 @@
 from selenium import webdriver
 
-
 driver = webdriver.Chrome(executable_path='C:\ChromeDriver\chromedriver.exe')
 driver.get('https://www.tut.by/')
 driver.maximize_window()
 
-#Working with Radio Buttons
+#Working with Radio Buttons (Check_Boxes is the same work)
 
 status = driver.find_element_by_xpath('//*[@id="aid18665"]').is_selected()   #False by default
 print(status)
